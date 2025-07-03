333c16e59717a3c135207c8e85e45689b676644c
Wil-dev <mehdididou7@icloud.com>
2025-07-03 20:13:02 +0200

    feat: ajouter la page de liste avec des composants de filtre et de liste de produits

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,5 +1,35 @@
+import Filter from "@/components/Filter/Filter";
+import ProductList from "@/components/ProductList/ProductList";
+import Image from "next/image";
+
 export default function Listpage() {
-  return (
-    <div>ListPage</div>
-  )
+	return (
+		<div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
+			{/* CAMPAIGN */}
+			<div className="bg-pink-50 hidden  px-4 sm:flex justify-between h-64">
+				<div className="w-2/3 flex flex-col items-center justify-center gap-8">
+					<h1 className="text-4xl font-semibold leading-[48px] text-gray-700">
+						Grab up to 50% off on <br />
+						Selected Products
+					</h1>
+					<button className='rounded-3xl bg-red-400 text-white w-max py-3 px-5 text-sm'>
+						Buy Now
+					</button>
+				</div>
+				<div className="relative w-1/3">
+					<Image
+						src="/woman.png"
+						alt=""
+						fill
+						objectFit="object-contain"
+					/>
+				</div>
+			</div>
+      {/* FILTER */}
+      <Filter />
+      {/* PRODUCTS */}
+      <h1 className="mt-12 text-xl font-semibold">Shoes For You</h1>
+      <ProductList />
+		</div>
+	);
 }
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,115 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Footer() {
-  return (
-    <div>Footer</div>
-  )
+	return (
+		<div className="py-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 bg-slate-200 text-sm mt-24">
+			{/* TOP */}
+			<div className="flex flex-col md:flex-row justify-between gap-24">
+				{/* LEFT */}
+				<div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+					<Link href="/">
+						<div className="text-2xl tracking-wide">Mehdidev7</div>
+					</Link>
+					<p>73, rue edouard vaillant, 93100 Montreuil, France</p>
+					<span className="font-semibold">hello@mehdidev7.dev</span>
+					<span className="font-semibold">+33 6 12 34 56 78</span>
+					<div className=" flex gap-6">
+						<Image src="/facebook.png" alt="Facebook" width={16} height={16} />
+						<Image
+							src="/instagram.png"
+							alt="Instagram"
+							width={16}
+							height={16}
+						/>
+						<Image src="/youtube.png" alt="YouTube" width={16} height={16} />
+						<Image
+							src="/pinterest.png"
+							alt="Pinterest"
+							width={16}
+							height={16}
+						/>
+						<Image src="/x.png" alt="X" width={16} height={16} />
+					</div>
+				</div>
+				{/* CENTER */}
+				<div className="hidden lg:flex justify-between w-1/2">
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">COMPANY</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="/">About Us</Link>
+              <Link href="/">Careers</Link>
+              <Link href="/">Affiliates</Link>
+              <Link href="/">Blog</Link>
+              <Link href="/">Contact Us</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">HELP</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="/">About Us</Link>
+              <Link href="/">Careers</Link>
+              <Link href="/">Affiliates</Link>
+              <Link href="/">Blog</Link>
+              <Link href="/">Contact Us</Link>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between">
+            <h1 className="font-medium text-lg">SUBSCRIBE</h1>
+            <div className="flex flex-col gap-6">
+              <Link href="/">About Us</Link>
+              <Link href="/">Careers</Link>
+              <Link href="/">Affiliates</Link>
+              <Link href="/">Blog</Link>
+              <Link href="/">Contact Us</Link>
+            </div>
+          </div>
+        </div>
+				{/* RIGHT */}
+				<div className="w-full md:w-1/2 lg:w-1/4 flex flex-col gap-8">
+					<h1>SUBSCRIBE</h1>
+					<p>Get the latest updates and offers.</p>
+					<div>
+						<input
+							type="text"
+							placeholder="Enter your email"
+							className="p-4 w-3/4"
+						/>
+						<button className="w-1/4 p-4 bg-red-400 text-white">JOIN</button>
+					</div>
+					<span className="font-semibold">Secure Payments</span>
+					<div className="flex justify-between">
+						<Image src="/discover.png" alt="Discover" width={40} height={20} />
+						<Image src="/skrill.png" alt="Skrill" width={40} height={20} />
+						<Image src="/paypal.png" alt="PayPal" width={40} height={20} />
+						<Image
+							src="/mastercard.png"
+							alt="MasterCard"
+							width={40}
+							height={20}
+						/>
+
+						<Image src="/paypal.png" alt="PayPal" width={40} height={20} />
+						<Image src="/visa.png" alt="Visa" width={40} height={20} />
+					</div>
+				</div>
+			</div>
+      {/* BOTTOM */}
+      <div className="flex flex-col md:flex-row justify-between items-center gap-8 mt-16">
+        <div>© 2023 Mehdidev7. All rights reserved.</div>
+        <div className="flex flex-col md:flex-row gap-8">
+          <div>
+            <span className="text-gray mr-4">Language</span>
+            <span className="font-medium">United States | English</span>
+          </div>
+          <div>
+            <span className="text-gray mr-4">Currency</span>
+            <span className="font-medium">$ USD</span>
+          </div>
+        </div>
+
+      </div>
+		</div>
+	);
 }
